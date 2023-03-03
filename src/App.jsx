b1137f1f7c67e9d30a8b25d57b072d72631c2513
cltl23 <charlietate232@hotmail.com>
2023-03-03 07:09:54 +0000

    Cleaned out default code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
