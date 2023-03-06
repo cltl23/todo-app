f4b7e8e4331f3535ab2c66d17e6267f6df5ab065
cltl23 <charlietate232@hotmail.com>
2023-03-06 08:16:54 +0000

    added todo section and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import Container from "../components/Container/Container";
+import Header from "../components/Header/Header";
+import AddTodo from "../components/Add_todo/AddTodo";
+import ListTodo from "../components/List_todo/ListTodo";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Header />
+      <AddTodo />
+      <ListTodo />
+      <Footer />
+    </Container>
+  );
 }
